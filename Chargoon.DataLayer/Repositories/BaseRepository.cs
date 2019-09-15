9fb86cb1b89f4c06e67b2df30d651009a46d8331
mobinhassani9374 <mahdihassani9374@gmail.com>
2019-09-15 13:48:59 +0430

    add BaseRepository

---
+++ Chargoon.DataLayer/Repositories/BaseRepository.cs
@@ -0,0 +1,39 @@
+﻿using Chargoon.DataLayer.Context;
+using Chargoon.DomainModels.Entities;
+using Chargoon.Utility;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chargoon.DataLayer.Repositories
+{
+    public abstract class BaseRepository<TEntity> where TEntity : BaseEntity
+    {
+        protected readonly AppDbContext _context;
+
+        public BaseRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public ServiceResult Create(TEntity entity)
+        {
+            _context.Add(entity);
+            return Save();
+        }
+
+        public ServiceResult Update(TEntity entity)
+        {
+            _context.Update(entity);
+            return Save();
+        }
+
+        public ServiceResult Save()
+        {
+            if (_context.SaveChanges() > 0) return ServiceResult.Okay();
+            return ServiceResult.Error();
+        }
+
+
+    }
+}
