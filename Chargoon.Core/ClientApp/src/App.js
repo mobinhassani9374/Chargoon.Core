6c2ffc0e274d5bda9fc6dca4a29f08b84f49348b
mahdihassani9374 <mahdihassani9374@gmail.com>
2019-09-15 13:27:22 +0430

    add compoennet logi and register

--- Chargoon.Core/ClientApp/src/App.js
+++ Chargoon.Core/ClientApp/src/App.js
@@ -1,14 +1,18 @@
 ﻿import React from 'react';
 import { Route } from 'react-router';
-import Layout from './components/Layout';
+// import Layout from './components/Layout';
 import Home from './components/Home';
 import Counter from './components/Counter';
 import FetchData from './components/FetchData';
+import Login from './components/Login';
+import Register from './components/Register';
 
 export default () => (
-  <Layout>
+  <div>
     <Route exact path='/' component={Home} />
     <Route path='/counter' component={Counter} />
+    <Route path='/login' component={Login} />
+    <Route path='/Register' component={Register} />
     <Route path='/fetchdata/:startDateIndex?' component={FetchData} />
-  </Layout>
+  </div>
 );
