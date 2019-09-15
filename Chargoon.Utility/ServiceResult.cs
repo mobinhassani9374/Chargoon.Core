53c3c211a7eb69e2274df52e29bdc8a604e2883d
mobinhassani9374 <mahdihassani9374@gmail.com>
2019-09-15 13:28:35 +0430

    add ServiceResult

---
+++ Chargoon.Utility/ServiceResult.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chargoon.Utility
+{
+    public class ServiceResult
+    {
+        public string Message { get; set; }
+
+        public bool IsSuccessed { get; set; }
+
+        public static ServiceResult Okay()
+        {
+            return new ServiceResult() {  IsSuccessed=true, Message="عملیات با موفقیت انجام شد"};
+        }
+
+        public static ServiceResult Error()
+        {
+            return new ServiceResult() { IsSuccessed = false, Message = "عملیات با خطا مواجه شد مجددا تلاش کنید" };
+        }
+    }
+}
