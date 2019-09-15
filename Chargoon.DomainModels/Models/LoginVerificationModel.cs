e46d9e730b5ccf4b386e052d22e0b275b7c2e46e
mobinhassani9374 <mahdihassani9374@gmail.com>
2019-09-15 15:12:20 +0430

    develop of verification

---
+++ Chargoon.DomainModels/Models/LoginVerificationModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Chargoon.DomainModels.Models
+{
+    public class LoginVerificationModel
+    {
+        [Required(ErrorMessage = "شماره همراه نمی تواند فاقد مقدار باشد")]
+        public string PhoneNumber { get; set; }
+
+        public int ActivationCode { get; set; }
+    }
+}
