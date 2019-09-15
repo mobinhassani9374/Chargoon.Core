0fe4e6c00f283821126fcf60e6e9ca1f57eac2de
mobinhassani9374 <mahdihassani9374@gmail.com>
2019-09-15 13:44:42 +0430

    develop of Login

---
+++ Chargoon.Core/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Chargoon.DomainModels.Dto;
+using Chargoon.DomainModels.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chargoon.Core.Controllers
+{
+    [Route("api/[controller]")]
+    public class AccountController : Controller
+    {
+        [HttpPost("[action]")]
+        public IActionResult Login(LoginModel loginModel)
+        {
+            return View();
+        }
+    }
+}
---
+++ Chargoon.DomainModels/Models/LoginModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chargoon.DomainModels.Models
+{
+    public class LoginModel
+    {
+        public string PhoneNumber { get; set; }
+    }
+}
