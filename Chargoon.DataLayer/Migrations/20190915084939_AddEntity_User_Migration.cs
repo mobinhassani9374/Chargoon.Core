b4eb717f3365727d98833309973f6fb5da5b3d70
mobinhassani9374 <mahdihassani9374@gmail.com>
2019-09-15 13:22:39 +0430

    AddEntity_User_Migration

---
+++ Chargoon.DataLayer/Migrations/20190915084939_AddEntity_User_Migration.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Chargoon.DataLayer.Migrations
+{
+    public partial class AddEntity_User_Migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    PhoneNumber = table.Column<string>(maxLength: 100, nullable: false),
+                    ActivationCode = table.Column<int>(nullable: false),
+                    CreateActivationCodeDate = table.Column<DateTime>(nullable: false),
+                    SerialNumber = table.Column<string>(maxLength: 100, nullable: false),
+                    FullName = table.Column<string>(maxLength: 10000, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_PhoneNumber",
+                table: "Users",
+                column: "PhoneNumber",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
