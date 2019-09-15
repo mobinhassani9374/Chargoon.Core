c5908400d45e7cab9605ce09b10fff1db889e89b
mobinhassani9374 <mahdihassani9374@gmail.com>
2019-09-15 13:50:24 +0430

    add UserRepository

---
+++ Chargoon.DataLayer/Repositories/UserRepository.cs
@@ -0,0 +1,16 @@
+﻿using Chargoon.DataLayer.Context;
+using Chargoon.DomainModels.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chargoon.DataLayer.Repositories
+{
+    public class UserRepository : BaseRepository<User>
+    {
+        public UserRepository(AppDbContext context) : base(context)
+        {
+
+        }
+    }
+}
