f80ceabe1be1b743b1f5c4e7a41b7b3b9f69a351
mobinhassani9374 <mahdihassani9374@gmail.com>
2019-09-15 12:26:38 +0430

    add BaseEntity

---
+++ Chargoon.Domains/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Chargoon.Domains
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
